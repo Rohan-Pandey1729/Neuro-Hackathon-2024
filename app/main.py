d2b041ce5d77ac0057302511ecc0726c59dbe351
Eric Ye <yuxuan.smiley@gmail.com>
2024-03-02 23:21:55 -0800

    Music thing

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file, send_from_directory
+import random
+from util.concentration import get_prob
 
 app = Flask(__name__)
 
@@ -37,6 +39,23 @@ def results():
     top_three_genres = set(sorted_genres[:3])
     print(top_three_genres)
     return render_template("results.html", genres=list(top_three_genres))
+
+@app.route("/test")
+def measure_music_concentration():
+    scores = request.args.get("scores", "").split(",")
+    if len(scores) == 10:
+        return str(max(map(float, scores)))
+    genre = genres[len(scores)]
+    music_file = f"{genre}.0000{random.randint(0, 9)}.wav"
+    return render_template("music_test.html", music_file="/music/" + genre + "/" + music_file)
+
+@app.route("/music/<path:filename>")
+def music(filename):
+    return send_from_directory("genre_clip_data", filename)
+
+@app.route("/get_concentration_score", methods=["POST"])
+def get_concentration_score():
+    return str(get_prob())
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
