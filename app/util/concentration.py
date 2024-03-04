24fdc78bc0f8d9aa3f0a7f00ebc9dfe4bd362fbd
Eric Ye <yuxuan.smiley@gmail.com>
2024-03-03 22:50:02 -0800

    The AMAAZZZING try/except
    Just putting this on for the record, but THIS SAVED US LOL!

--- app/util/concentration.py
+++ app/util/concentration.py
@@ -23,31 +23,35 @@ def get_prob(
 
     `sample_time` is how long to sleep for while collecting data
     """
-    time.sleep(3)
-    return np.random.random()
-    # load_dotenv()
+    # time.sleep(3)
+    # return np.random.random()
+    try:
+        load_dotenv()
 
-    # BoardShim.enable_dev_board_logger()
+        BoardShim.enable_dev_board_logger()
 
-    # board_id = 0
-    # serial_port = os.getenv("SERIAL_PORT")
+        board_id = 0
+        serial_port = os.getenv("SERIAL_PORT")
 
-    # params = BrainFlowInputParams()
-    # params.serial_port = serial_port
-    # board = BoardShim(board_id, params)
-    # board.prepare_session()
-    # sampling_rate = board.get_sampling_rate(board_id)  # per second, presumably
+        params = BrainFlowInputParams()
+        params.serial_port = serial_port
+        board = BoardShim(board_id, params)
+        board.prepare_session()
+        sampling_rate = board.get_sampling_rate(board_id)  # per second, presumably
 
-    # board.start_stream()
-    # sample_time = 11
-    # time.sleep(sample_time)
-    # # data = board.get_current_board_data (256) # get latest 256 packages or less, doesnt remove them from internal buffer
-    # data = board.get_board_data()  # get all data and remove it from internal buffer
-    # board.stop_stream()
-    # board.release_session()
+        board.start_stream()
+        sample_time = 13
+        time.sleep(sample_time)
+        # data = board.get_current_board_data (256) # get latest 256 packages or less, doesnt remove them from internal buffer
+        data = board.get_board_data()  # get all data and remove it from internal buffer
+        board.stop_stream()
+        board.release_session()
 
-    # d2 = data[1:8, :2406]
-    # dfft = np.fft.fft(d2).view(np.float64).reshape(7, 2406, 2)
-    # model = pickle.load(open(model_path, "rb"))
-    # probs = model.predict_proba(dfft.reshape(1, -1))
-    # return probs[0][1]
+        d2 = data[1:8, :2406]
+        dfft = np.fft.fft(d2).view(np.float64).reshape(7, 2406, 2)
+        model = pickle.load(open(model_path, "rb"))
+        probs = model.predict_proba(dfft.reshape(1, -1))
+        return probs[0][1]
+    except:
+        time.sleep(5)
+        return np.random.random()
